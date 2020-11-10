b18cdb20514d800dc774f8d8ebf35398bb0dabab
obarrott <71147609+obarrott@users.noreply.github.com>
2020-11-10 11:44:00 -0700

    initial commit

---
+++ MessengerPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessengerPractice
+//
+//  Created by Owen Barrott on 11/10/20.
+//  Copyright © 2020 Owen Barrott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
